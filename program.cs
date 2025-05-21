a91f7866ed99f771311f2c613042d2bfd1a91f04
kemilyyyy <kemilyteixeira3004@gmail.com>
2025-05-21 16:42:33 +0000

    Create mensagem

--- program.cs
+++ program.cs
@@ -1,1 +1,21 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICanal whatsapp = new WhatsApp("+551199999999");
+        ICanal telegram = new Telegram("+551188888888", "pedro_bot");
+        ICanal facebook = new Facebook("pedro.fb");
+        ICanal instagram = new Instagram("pedro.insta");
+
+        Mensagem msgTexto = new TextoMensagem("Olá, tudo bem?");
+        Mensagem msgFoto = new MensagemFoto("Olha essa foto", "foto1.jpg", "jpg");
+        Mensagem msgVideo = new MensagemVideo("Assista isso!", "video.mp4", "mp4", 45.2);
+        Mensagem msgArquivo = new MensagemArquivo("Segue o arquivo", "documento.pdf", "pdf");
+
+        whatsapp.EnviarMensagem(msgTexto);
+        telegram.EnviarMensagem(msgFoto);
+        facebook.EnviarMensagem(msgVideo);
+        instagram.EnviarMensagem(msgArquivo);
+    }
+}
 
